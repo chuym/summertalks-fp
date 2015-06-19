788f8d595a91e6ec9ec467c1a1a5d35fa6cdcb4c
Chuy Del Castillo <chuy@imchuy.com>
2015-06-19 15:06:05 -0700

    Adding tests for second exercise.

--- part1/js/tests.js
+++ part1/js/tests.js
@@ -1,52 +1,47 @@
 var assert = require("assert"),
-    rt1 = require("./rt1");
+    ex1 = require("./ex1"),
+    ex2 = require("./ex2");
 
-describe("Referential Transparency exercise", function () {
-
-    it("Expect function to receive an array and a look up value", function () {
-        assert.throws(function () {
-            rt1('not good value', []);
-        }, /Function expects an array and a lookup value/);
-    });
+describe("Exercise #1", function () {
 
     it("Should find the correct result for a simple case", function () {
         var case = [5,4,1],
-            result = rt1(case, 4);
+            result = ex1(case, 4);
 
         assert.equal(result, 1);
     });
 
     it("Should handle an array object properly", function () {
         var case = new Array(5,4,1),
-            result = rt1(case, 4);
+            result = ex1(case, 4);
 
         assert.equal(result, 1);
     });
 
     it("Should return undefined if index is not found", function () {
         var case = [5,4,1],
-            result = rt1(case, 7);
+            result = ex1(case, 7);
 
         assert.equal(result, undefined);
     });
 
     it("Should return undefined if index is the last one", function () {
         var case = [5,4,1],
-            result = rt1(case, 1);
+            result = ex1(case, 1);
 
         assert.equal(result, undefined);
     });
 
     it("If there are multiple occurences, use the last one.", function () {
         var case = [5,4,1,4,5],
-            result = rt1(case, 4);
+            result = ex1(case, 4);
 
         assert.equal(result, 5);
     });
 
     it("If there are multiple occurences, use the last one even if it's the last one on the list.", function () {
         var case = [5,4,1,4,5],
-            result = rt1(case, 5);
+            result = ex1(case, 5);
 
         assert.equal(result, undefined);
     });
@@ -55,7 +50,7 @@ describe("Referential Transparency exercise", function () {
 
     it("Should work for strings as well", function () {
         var case = ["apple", "orange", "banana"]
-            result = rt1(case, "orange");
+            result = ex1(case, "orange");
 
         assert.equal(result, "banana");
     });
@@ -72,8 +67,36 @@ describe("Referential Transparency exercise", function () {
                 val: 3
             }
         ]
-            result = rt1(case, { val: 1 });
+            result = ex1(case, { val: 1 });
 
         assert.deepEqual(result, { val: 2 });
     });
+});
+
+describe("Exercise #2", function () {
+    it("Should return the correct result for a simple matrix", function () {
+        var case = [ [1,2,3], [4,5,6], [7,8,9] ];
+        assert.deepEqual(ex2(case), case);
+    });
+
+    it("Should return the correct result for a recatngular matrix", function () {
+        var case = [ [1,-5,3], [4,5,6] ];
+        assert.deepEqual(ex2(case), [ [4,5,6] ]);
+    });
+
+    it("Should return a vertical matrix as a correct result", function () {
+        var case = [ [-6, 15, 2], [-4,5,-9], [1, 19, -13] ];
+        assert.deepEqual(ex2(case), [ [15,5,19] ]);
+    });
+
+    it("Should return a partial vertical matrix as a correct result", function () {
+        var case = [ [-6, -15, 2], [-4,5,-9], [1, 19, -13] ];
+        assert.deepEqual(ex2(case), [ [5,19] ]);
+    });
+
+    it("Should return a sub matrix around the center", function () {
+        var case = [ [-6, -15, 2, -17], [-4,5,19, 39], [1, 19, 33, -25], [1, 4, 5, 2] ];
+        assert.deepEqual(ex2(case), [ [5, 19], [19, 33] ]);
+    });
+
 });
